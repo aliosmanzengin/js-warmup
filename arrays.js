386245d34492d9651f4f858aadb9d4054d203c94
Ali Osman Zengin <66180039+aliosmanzengin@users.noreply.github.com>
2022-02-10 11:20:10 +0300

    arrays

--- arrays.js
+++ arrays.js
@@ -37,3 +37,43 @@ console.log(friendList);//['sue', 'tim', 'pete', empty, 'Gabe', empty × 2, 'Ale
 
 //We should use array methods to add data
 
+// to add the array we use:
+// 1) push() : add data at the end of the array
+friendList.push("George")
+
+console.log(friendList);//['sue', 'tim', 'pete', empty, 'Gabe', empty × 2, 'Alex', 'George']
+
+// 2) unshift() : add data at the BEGINNING
+friendList.unshift('Michelle')
+
+//DELETING ELEMENTS FROM ARRAY
+
+// 3) pop() : delete the LAST element
+friendList.pop();//if array is emty, undefined will return
+
+// 4) shift() : delete the FIRST data
+friendList.shift();
+
+var ages = [27, 77, 45, 11, 77,555,222, 77]
+console.log(typeof(ages));//object
+
+//because array data type is object, you can add different types of data into the array
+
+var myArray = [27, "Loui", true, ages]
+
+// 5) indexOf(item, from) : returns 1st index of the item
+console.log(ages.indexOf(45));//2
+console.log(ages.indexOf(true));//-1
+console.log(ages.indexOf(77,2));//4 --> start search from index 2
+
+// 6) lastIndexOf(item, from) 
+console.log(ages.lastIndexOf(77)); //7
+
+// 7) includes(item, starting index)
+//returns true if item is inside the array
+//returns false if not found
+console.log(ages.includes(45));
+
+
+
+
